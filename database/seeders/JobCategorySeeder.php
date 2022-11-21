7ef1337314704e49ff2fb3a7154234a78d8dafc6
Jeedie JIDE <enockyona906@gmail.com>
2022-11-21 08:53:23 +0300

    add other job categories

--- database/seeders/JobCategorySeeder.php
+++ database/seeders/JobCategorySeeder.php
@@ -17,8 +17,44 @@ class JobCategorySeeder extends Seeder
     public function run()
     {
         $job =  JobCategory::updateOrCreate([
-            'name' => 'Gown',
-            'slug' => Str::slug('Gown'),
+            'name' => 'Backend Developer',
+            'slug' => Str::slug('Backend Developer'),
+        ]);
+        $job =  JobCategory::updateOrCreate([
+            'name' => 'Frontend Developer',
+            'slug' => Str::slug('Frontend Developer'),
+        ]);
+        $job =  JobCategory::updateOrCreate([
+            'name' => 'Software Engineer',
+            'slug' => Str::slug('Software Engineer'),
+        ]);
+        $job =  JobCategory::updateOrCreate([
+            'name' => 'Software Developer',
+            'slug' => Str::slug('Software Developer'),
+        ]);
+        $job =  JobCategory::updateOrCreate([
+            'name' => 'Full Stack Software Developer',
+            'slug' => Str::slug('Full Stack Software Developer'),
+        ]);
+        $job =  JobCategory::updateOrCreate([
+            'name' => 'Network Engineer',
+            'slug' => Str::slug('Network Engineer'),
+        ]);
+        $job =  JobCategory::updateOrCreate([
+            'name' => 'Database Developer',
+            'slug' => Str::slug('Database Developer'),
+        ]);
+        $job =  JobCategory::updateOrCreate([
+            'name' => 'Blockchain Developer',
+            'slug' => Str::slug('Blockchain Developer'),
+        ]);
+        $job =  JobCategory::updateOrCreate([
+            'name' => 'CyberSecurity Engineer',
+            'slug' => Str::slug('CyberSecurity Engineer'),
+        ]);
+        $job =  JobCategory::updateOrCreate([
+            'name' => 'Cloud Security Engineer',
+            'slug' => Str::slug('Cloud Security Engineer'),
         ]);
     }
 }
