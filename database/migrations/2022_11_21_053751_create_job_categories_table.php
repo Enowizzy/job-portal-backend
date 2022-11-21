0341192d7fcce1fd5a3f5f904b762fa4296699c4
Jeedie JIDE <enockyona906@gmail.com>
2022-11-21 08:55:20 +0300

    job categories migration commit

--- database/migrations/2022_11_21_053751_create_job_categories_table.php
+++ database/migrations/2022_11_21_053751_create_job_categories_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('job_categories', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
